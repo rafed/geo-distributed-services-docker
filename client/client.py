3d297e8c4b680dc23123777e9dce663e8bcb3dde
Rafed Muhammad Yasir <rafed123@yahoo.com>
2021-05-02 15:41:09 +0600

    Geo distributed server

--- client/client.py
+++ client/client.py
@@ -1,11 +1,36 @@
 import requests
 import time
 
+def (detectfromGPSLocation):
+    if GPS == Dhaka:
+        hit.(dhaka.server.com)
+    elif GPS == Chittagong:
+        hit.(chittagong.server.com)
+
+url = "http://dhaka.server.com"
+
 for i in range(1000):
     if i%2 == 0:
-        r = requests.get(f"http://localhost:8080/api/v1/{i}")
+        r = requests.get(f"{url}/api/v1/{i}")
     else:
-        r = requests.get(f"http://localhost:8080/api/v2/{i}")
+        r = requests.get(f"{url}/api/v2/{i}")
 
     print(r.text)
     time.sleep(1)
+
+
+
+email_list = [omukh.com, omukh.com, omukh.com, omukh.com, omukh.com, omukh.com]
+
+rabbit_mq
+    email = [email,.com, email.com]
+
+
+
+def add_to_email_list(email):
+    rabbit_mq.append(email)
+
+def sendEmail():
+    email = rabbit_mq.get(email)
+    if email_list != None:
+        email_list[0].send()
